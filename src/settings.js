02109078b36949ce2337480017b3ac058ea10e44
mttly <2183033302@qq.com>
2022-09-18 17:07:39 +0800

    人资项目初始化

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // 标题
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false, // 是否固定头部
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false// 是否显示logo
+}
